69e0c1927a1833796b358e7a5d074ee6c0ba1c6a
ZOID415 <michaelalfaro93@gmail.com>
2023-02-13 17:24:19 -0800

    adds working code to publish answers and create README

--- index.js
+++ index.js
@@ -1,102 +1,129 @@
 // TODO:  Make sure you add your dependencies here
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const createMD = require("./utils/generateMarkdown");
 
-const generateMarkdown = ({ description, location, linkedin, github }) =>
-  // TODO:  Destructure the the object you pass to this function, which you will have to add inside the parentheses so you can reference the values in this template literal
-  `# <Your-Project-Title>
+// const generateMarkdown = ({ description, location, linkedin, github }) =>
+//   // TODO:  Destructure the the object you pass to this function, which you will have to add inside the parentheses so you can reference the values in this template literal
+//   `# <Your-Project-Title>
 
-## Description
+// ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+// Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+// - What was your motivation?
+// - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+// - What problem does it solve?
+// - What did you learn?
 
-## Table of Contents (Optional)
+// ## Table of Contents (Optional)
 
-If your README is long, add a table of contents to make it easy for users to find what they need.
+// If your README is long, add a table of contents to make it easy for users to find what they need.
 
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [Credits](#credits)
+// - [License](#license)
 
-## Installation
+// ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
-## Usage
+// ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+// Provide instructions and examples for use. Include screenshots as needed.
 
-To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+// To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
 
     
 
-## Credits
+// ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
+// List your collaborators, if any, with links to their GitHub profiles.
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
-If you followed tutorials, include links to those here as well.
+// If you followed tutorials, include links to those here as well.
 
-## License
+// ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
----
+// ---
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+// 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
+// ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+// ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+// Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
-## Features
+// ## Features
 
-If your project has a lot of features, list them here.
+// If your project has a lot of features, list them here.
 
-## How to Contribute
+// ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+// If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
-## Tests`;
+// ## Tests`;
 
 inquirer
   .prompt([
     {
       type: "input",
-      name: "description",
-      message: "What was your motivation?",
+      name: "title",
+      message: "What is your title?",
     },
     {
       type: "input",
       name: "description",
-      message: "Why did you build this project?",
+      message: "Give a brief description of your project",
     },
     {
       type: "input",
-      name: "description",
-      message: "What problem does it solve?",
+      name: "github",
+      message: "What is your github username?",
     },
     {
       type: "input",
-      name: "description",
-      message: "What did you learn?",
+      name: "email",
+      message: "What is your email?",
     },
-    
+    {
+      type: "list",
+      name: "license",
+      message: "Choose one of the licenses",
+      choices: ["MIT", "APACHE-2.0", "AFL-3.0", "APL-1.0", "None"],
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How to use this project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How to install this project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "How to test this project?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "How can others contribute to this project?",
+    },
   ])
-  
 
   // TODO: Create a .then() with a call back function that takes the user's responses, generates HTML, and writes the file as "index.html" to the disk.
   .then((answers) => {
-    const readMeContent = generateMarkdown(answers);
+    console.log("answers= ", answers);
+    const readMeContent = createMD(answers);
 
     fs.writeFile("README.md", readMeContent, (err) =>
       err ? console.log(err) : console.log("Success!")
